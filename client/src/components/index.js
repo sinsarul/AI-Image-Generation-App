fd03bf42c1e6d62ca699586a4e513a62f63260fb
sinsar11456 <sinsarsanu111@gmail.com>
2023-12-21 21:40:44 +0530

    Add existing project files to Git

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import Card from "./Card";
+import FormField from "./FormField.jsx";
+import Loader from "./Loader";
+
+
+
+export {
+    Card,
+    Loader, 
+    FormField
+}
---
+++ client/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+export default defineConfig({
+  plugins: [],
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+});
