c47c4376d4e281c50e47cb3e5f113c79b73d0807
sinsar11456 <sinsarsanu111@gmail.com>
2023-12-27 10:19:26 +0530

    last

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,65 @@
-import { React, useState, useEffect } from "react";
-import { Card , FormField , Loader } from "../components";
+import React, { useState, useEffect } from "react";
 
-const RenderCards = ({ data, title }) => {
+import { Card, FormField, Loader } from "../components";
+
+const RenderCards = ({data, title}) => {
   if (data?.length > 0) {
     return data.map((post) => <Card key={post._id} {...post} />);
   }
   return (
     <h2 className="mt-5 font-bold text-[#6449ff] text-xl upperCase">{title}</h2>
   );
-};  
+};
 
 const Home = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http: //localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -27,7 +72,14 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-16">
-        <FormField />
+        <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search something..."
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className="mt-10">
@@ -45,9 +97,12 @@ const Home = () => {
             )}
             <div className="grid lg:grid-cols4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards data={[]} title="No search result found" />
+                <RenderCards
+                  data={searchedResults}
+                  title="No search result found"
+                />
               ) : (
-                <RenderCards data={[]} title="No posts found" />
+                <RenderCards data={allPosts} title="No posts found" />
               )}
             </div>
           </>
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const openai = new OpenAI({
-  apikey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 router.route("/").get((req, res) => {
@@ -18,20 +18,21 @@ router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
 
-    const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse?.data?.data?.[0]?.b64_json || "DefaultFallbackImage";
+
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
     res
       .status(500)
-      .send(error?.response.data.error.message || "Something went wrong");
+      .send(error?.response?.data?.error?.message || "Something went wrong");
   }
 });
 
